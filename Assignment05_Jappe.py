019ad7057b58c0ccb11f192423eeeeef60f8fe38
williamjappe <jappe.w@northeastern.edu>
2024-03-04 22:24:10 -0500

    Added hypotenuse and area methods and made some fixes in syntax

--- Assignment05_Jappe.py
+++ Assignment05_Jappe.py
@@ -10,16 +10,16 @@ class BasicMathOperations:
     #Method for adding
     def Add(self):
         print("What is the first number?")
-        self.firstnumber = input()
+        self.firstnumber = float(input())
         print("What is the second number?")
-        self.secondnumber = input()
+        self.secondnumber = float(input())
         self.sum = self.firstnumber + self.secondnumber
         print("The sum is", self.sum)
     
     #Method for operations
     def Operation(self, firstnumber, secondnumber, operation):
-        self.firstnumber = firstnumber
-        self.secondnumber = secondnumber
+        self.firstnumber = float(firstnumber)
+        self.secondnumber = float(secondnumber)
         if operation == "add":
             self.result = self.firstnumber + self.secondnumber
         elif operation == "subtract":
@@ -55,4 +55,22 @@ class BasicMathOperations:
         for x in range(self.startnumber, self.endnumber + 1):
             print (x)
     
+    #Method for hypotenuse
+    def calculateHypotenuse(self, base, perpendicular):
+        self.base = base
+        self.perpendicular = perpendicular
+        def calculateSquare(number):
+            square = number ** 2
+            return square
+        self.hypotenuse = (calculateSquare(self.base) + calculateSquare(self.perpendicular)) ** 0.5
+        print("The hypotenuse is", self.hypotenuse)
     
+    #Method for area
+    def Area (self, width, height):
+        self.width = width
+        self.height = height
+        self.area = self.width * self.height
+        print("The area of the rectangle is", self.area)
+    
+    
+test = BasicMathOperations()
