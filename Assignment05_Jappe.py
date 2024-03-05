8d2a1f7732dc02a15190d83d40c9c86b60a3a995
williamjappe <jappe.w@northeastern.edu>
2024-03-04 21:52:59 -0500

    Added operations and squaring method and also fixed adding method

--- Assignment05_Jappe.py
+++ Assignment05_Jappe.py
@@ -1,12 +1,41 @@
+#Main class for operations
 class BasicMathOperations:
     
+    #Method for greeting
     def Greet(self, firstname, lastname):
         self.firstnae = firstname
         self.lastname = lastname
         print("Nice to meet you", self.firstname, self.lastname)
     
-    def Add(self, firstnumber, secondnumber):
-        self.firstnumber = firstnumber
-        self.secondnumber = secondnumber
+    #Method for adding
+    def Add(self):
+        print("What is the first number?")
+        self.firstnumber = input()
+        print("What is the second number?")
+        self.secondnumber = input()
         self.sum = self.firstnumber + self.secondnumber
         print("The sum is", self.sum)
+    
+    #Method for operations
+    def Operation(self, firstnumber, secondnumber, operation):
+        self.firstnumber = firstnumber
+        self.secondnumber = secondnumber
+        if operation == add:
+            self.result = self.firstnumber + self.secondnumber
+        elif operation == subtract:
+            self.result = self.firstnumber - self.secondnumber
+        elif operation == multiply:
+            self.result = self.firstnumber * self.secondnumber
+        elif operation == divide:
+            self.result = self.firstnumber / self.secondnumber
+        else:
+            print("Error! Operation not recognized!")
+            return False
+        print("The result is", self.result)
+        return self.result
+    
+    #Method for squaring
+    def Square(self, number):
+        self.number = number
+        self.square = self.number ** 2
+        print("The square of the number is," self.square)
